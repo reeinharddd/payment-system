72f1850e18398da03f801c4e2cdd940b88d10b82
Erik <beltraberik@gmail.com>
2025-11-27 19:04:38 -0800

    chore: update configuration files for monorepo consistency
    - Update lint-staged configuration
    - Improve commitlint rules
    - Align ESLint configs across apps
    - Update TypeScript configurations
    - Ensure Angular config consistency
    
    Type: chore (configuration updates)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'type-enum': [
+    "type-enum": [
       2,
-      'always',
+      "always",
       [
-        'feat',     // New feature
-        'fix',      // Bug fix
-        'docs',     // Documentation
-        'style',    // Formatting, missing semi colons, etc
-        'refactor', // Code change that neither fixes a bug nor adds a feature
-        'perf',     // Performance improvements
-        'test',     // Adding missing tests
-        'chore',    // Build process, aux tools
-        'revert',   // Revert a commit
-        'ci',       // CI related changes
-        'build'     // Build system or external dependencies
+        "feat", // New feature
+        "fix", // Bug fix
+        "docs", // Documentation
+        "style", // Formatting, missing semi colons, etc
+        "refactor", // Code change that neither fixes a bug nor adds a feature
+        "perf", // Performance improvements
+        "test", // Adding missing tests
+        "chore", // Build process, aux tools
+        "revert", // Revert a commit
+        "ci", // CI related changes
+        "build", // Build system or external dependencies
       ],
     ],
-    'scope-case': [2, 'always', 'kebab-case'],
+    "scope-case": [2, "always", "kebab-case"],
   },
 };
